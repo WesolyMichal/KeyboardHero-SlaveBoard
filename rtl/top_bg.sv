bcf940ab9e2be5d84f7ead6d995aa9c86f052140
Michal Wesolowski <michalwes@poczta.onet.pl>
2026-06-30 16:13:35 +0200

    top_bg update

--- rtl/top_bg.sv
+++ rtl/top_bg.sv
@@ -1,32 +1,32 @@
+import vga_pkg::*;
+import game_pkg::*;
+
 module top_bg (
     input logic clk,
     input logic rst_n,
 
-    input [7:0] button,
+    input enter_in_FSM,
+    input [1:0] master_song,
     input logic [15:0] score_in,
-    input logic enable_start,
-    input logic enable_song_choose,
-    input logic enable_song,
-    input logic enable_endscreen,
+    input game_pkg::enable_bgs enable_backgrounds,
 
     input vga_if vga_in,
-    output vga_if vga_out
+    output vga_if vga_out,
+    output logic enable_song
 );
 
-import vga_pkg::*;
+game_pkg::enable_bgs enable_from_bg;
+wire [11:0] rgb_out_start_bg, rgb_out_choose_bg, rgb_out_song_bg, rgb_out_endscreen_bg;
 
-wire enable_start_out, enable_choose_out, enable_song_out, enable_endscreen_out;
-wire [11:0] rgb_out_start_bg, rgb_out_choose_bg, rgb_out_song_bg, rgb_out_endscreen_bg; 
-
-vga_if delay_vga_out; 
+vga_if delay_vga_out;
 
 start_bg u_start_bg (
     .clk,
     .rst_n,
-    .button,
-    .enable_start_in(enable_start),
+    .enter(enter_in_FSM),
+    .enable_start_in(enable_backgrounds.enable_start),
     .vga_in(vga_in),
-    .enable_start_out,
+    .enable_start_out(enable_from_bg.enable_start),
     .rgb_out_start_bg
 );
 
@@ -34,19 +34,18 @@ song_choose_bg u_song_choose_bg (
     .clk,
     .rst_n,
     .vga_in(vga_in),
-    .button,
-    .enable_choose_in(enable_song_choose),
-    .enable_choose_out,
-    .rgb_out_choose_bg,
-    .selected_song()
+    .master_song(master_song),
+    .enable_choose_in(enable_backgrounds.enable_song_choose),
+    .enable_choose_out(enable_from_bg.enable_song_choose),
+    .rgb_out_choose_bg
 );
 
 song_bg u_song_bg (
     .clk,
     .rst_n,
-    .enable_song_in(enable_song),
+    .enable_song_in(enable_backgrounds.enable_song),
     .vga_in(vga_in),
-    .enable_song_out,
+    .enable_song_out(enable_from_bg.enable_song),
     .rgb_out_song_bg
 );
 
@@ -54,9 +53,9 @@ endscreen_bg u_endscreen_bg(
     .clk,
     .rst_n,
     .end_score_in(score_in),
-    .enable_endscreen_in(enable_endscreen),
+    .enable_endscreen_in(enable_backgrounds.enable_endscreen),
     .vga_in(vga_in),
-    .enable_endscreen_out,
+    .enable_endscreen_out(enable_from_bg.enable_endscreen),
     .rgb_out_endscreen_bg
 );
 
@@ -73,16 +72,13 @@ delay #(
 mux_bg u_mux_bg (
     .clk,
     .rst_n,
-    .enable_start(enable_start_out),
+    .enable_from_bg,
     .rgb_start(rgb_out_start_bg),
-    .enable_song_choose(enable_choose_out),
     .rgb_choose(rgb_out_choose_bg),
-    .enable_song(enable_song_out),
     .rgb_song(rgb_out_song_bg),
-    .enable_endscreen(enable_endscreen_out),
     .rgb_endscreen(rgb_out_endscreen_bg),
     .delay_vga_in(delay_vga_out),
-    .enable_song_out(enable_song_mux),
+    .enable_song_out(enable_song),
     .vga_out(vga_out)
 );
 
